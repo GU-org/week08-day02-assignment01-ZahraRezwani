9feb9d1266bed0465e93e7465c8fb269aee91017
Zahra Rezvani <zrarezvani1385.com@gmail.com>
2026-03-04 05:45:25 +0430

    changes

--- Week08/Day02/Assignment01/index.js
+++ Week08/Day02/Assignment01/index.js
@@ -2,3 +2,19 @@ function bmiCalc(w,h){
     bmi=w/(h*h);
     return bmi;
 }
+let weight =prompt("please enter your weight in kg.");
+let height= prompt("please enter your height in m.");
+let result= bmiCalc(weight,height);
+if(result<18.5){
+    alert("you are skinny.");
+}
+if(result>18.5 && result<=24.9){
+    alert("you are normal.");
+}
+if(result>24.9 && result<=29.9){
+    alert("you have overweight.");
+}
+if(result>29.9){
+    alert("you are fat.");
+}
+bmiCalcm()
